4bbd500d45918169c237e7dc8dbb6288b12d27a0
Sakeb Ahmad <sakebahmad7461@gmail.com>
2022-07-08 10:05:16 +0530

    java initiate

--- arrayEx.java
+++ arrayEx.java
@@ -10,11 +10,12 @@ public class arraysEx{
      students[1] = "Lucy";
      students[2] = "Emma";
      students[3] = "John";
-     
+     students[3] = "Sakeb";
+
      System.out.println(students[2]);
 
      //another way to declare an array
-     String names[] = {"Adam","Lucy","Emma","John"};
+     String names[] = {"Sakeb","Adam","Lucy","Emma","John"};
      System.out.println(names[3]);
 
      //throws an exception
---
+++ ex1.java
@@ -0,0 +1,12 @@
+class Coffee {
+    protected void order() {
+        System.out.println("Coffee");
+    }
+}
+
+public class Cappuccino extends Coffee {
+    public static void main(String args[]) {
+        Cappuccino c = new Cappuccino();
+        c.order();
+    }
+}
